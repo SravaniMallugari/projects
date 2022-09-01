1ed4a8b99447f1477611e1276bec42c207591b74
SravaniMallugari <111357901+SravaniMallugari@users.noreply.github.com>
2022-09-01 16:55:37 +0530

    Add files via upload

---
+++ myassign/studentdata/studentdata/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft. VisualBasic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentdata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String data = System.IO.File.ReadAllText(@"C:\Users\Texas\myassign\studentdata.txt");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            System.Console.WriteLine("Rainbow school students report: " +data);
+            Console.ReadLine();
+        }
+    }
+}
